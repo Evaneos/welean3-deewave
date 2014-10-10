4f638d48c00b7b625ee193ab6541e94baa31e12c
Christophe Hurpeau <christophe@hurpeau.com>
2014-10-10 12:19:36 +0200

    improvements server

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -1,15 +1,24 @@
 require('./init');
+var fs = require('fs');
 var koa = require('koa');
 var serve = require('koa-static');
 var request = require('koa-request');
+var errorsParser = require('springbokjs-errors');
+var ErrorHtmlRenderer = require('springbokjs-errors/htmlRenderer');
+var errorHtmlRenderer = new ErrorHtmlRenderer();
 var app = koa();
 
 var argv = require('minimist')(process.argv.slice(2));
 
-app.use(function *(next){
-    yield next;
-    console.log('%s %s - %sms', this.method, this.url, this._requestTook);
+
+process.on('uncaughtException', function(err) {
+    try {
+        errorsParser.log(err);
+    } catch (err2) {
+        console.error(err2.stack);
+    }
 });
+
 
 // logger
 
@@ -21,10 +30,10 @@ app.use(function *(next){
 // x-response-time
 
 app.use(function *(next){
-  var start = new Date();
+  var start = Date.now();
   this._requestStartedAt = start;
   yield next;
-  var ms = new Date() - start;
+  var ms = Date.now() - start;
   this._requestTook = ms;
   this.set('X-Response-Time', ms + 'ms');
 });
@@ -47,4 +56,11 @@ app.use(function *() {
 
 
 var port = argv.port || 3000;
-app.listen(port, console.log.bind(null, 'Listening on port ' + port));
+
+app.listen(argv['socket-path'] || port, function() {
+    if (argv['socket-path']) {
+        fs.chmodSync(argv['socket-path'], '777');
+    }
+    logger.log('Listening on '
+            +(argv['socket-path'] ? ' socket path ' + argv['socket-path'] : 'port ' + port));
+});
