037aa1d0e1f9121b79816de9e1a24e29f9df2e2f
cyrilf <cyrilf7x@gmail.com>
2014-10-10 15:50:59 +0200

    Code header ;)

--- app/src/server/server.js
+++ app/src/server/server.js
@@ -1,3 +1,17 @@
+/*
+
+8888888b.                    888       888
+888  "Y88b                   888   o   888
+888    888                   888  d8b  888
+888    888  .d88b.   .d88b.  888 d888b 888  8888b.  888  888  .d88b.
+888    888 d8P  Y8b d8P  Y8b 888d88888b888     "88b 888  888 d8P  Y8b
+888    888 88888888 88888888 88888P Y88888 .d888888 Y88  88P 88888888
+888  .d88P Y8b.     Y8b.     8888P   Y8888 888  888  Y8bd8P  Y8b.
+8888888P"   "Y8888   "Y8888  888P     Y888 "Y888888   Y88P    "Y8888
+
+                                                > Draw, listen, enjoy!
+*/
+
 require('./init');
 var fs = require('springbokjs-utils/fs');
 var koa = require('koa');
