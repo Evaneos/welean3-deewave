5f96d6ca5f6c08d110c5bd3d5e7292b0e7a633b2
Christophe Hurpeau <christophe@hurpeau.com>
2014-10-11 21:01:43 +0200

    use spotify play list

--- app/src/browser/js/PlayerManager.js
+++ app/src/browser/js/PlayerManager.js
@@ -1,36 +1,21 @@
 class PlayerManager {
-	display (tracks) {
-		var accessToken = $.QueryString['accessToken'];
-		var results = document.querySelector('.player-playlist');
-		var spotifyIds = tracks.map( (track) => track.foreign_id.substring(14) ).reduce( (acc, id) => (acc) ? acc + ',' + id : id);
-		console.log(spotifyIds);
-		$.ajax({
-			method: 'GET',
-			url: 'https://api.spotify.com/v1/tracks',
-			headers: (accessToken) ? {'Authorization': 'Bearer ' + accessToken } : {},
-			data: {
-				ids: spotifyIds
-			},
-			success: function (response) {
-				console.log('response', response);
-				results.innerHTML = '';
-				var fragment = document.createDocumentFragment();
-				response.tracks.forEach(function (track) {
+    display(tracks) {
+        //var accessToken = $.QueryString['accessToken'];
+        //var playerContainer = document.getElementById('player-container');
+        var playerLoading = document.getElementById('player-loading');
+        var player = document.getElementById('player');
+        playerLoading.removeAttribute('style');
 
-					var item = document.createElement('tr');
+        player.innerHTML = '';
 
-					var tpl = '';
-					tpl+= '<td><a href="' + track.external_urls.spotify + '"><img src="' + track.album.images[2].url + '"></a></td>';
-					tpl+= '<td>' + track.name + '</td>';
-
-					item.innerHTML = tpl;
-
-					fragment.appendChild(item);
-				});
-				results.appendChild(fragment);
-			}
-		});
-	}
+        var spotifyIds = tracks.map((track) => track.foreign_id.substring(14) );
+        var div = document.createElement('div');
+        div.innerHTML = '<iframe width="640" height="420" src="https:/'
+                + '/embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:'
+                + spotifyIds.join(',') + '" frameborder="0" allowtransparency="true"></iframe>';
+        player.appendChild(div);
+        playerLoading.setAttribute('style', 'display: none');
+    }
 }
 
 module.exports = new PlayerManager();
